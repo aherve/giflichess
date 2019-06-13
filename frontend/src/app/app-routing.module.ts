62782adcec21b607281d591064b4b72964a7362c
Aurélien Hervé <mail@aurelien-herve.com>
2019-06-13 18:50:02 +0200

    bootstrap angular app

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from 'src/app/home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
