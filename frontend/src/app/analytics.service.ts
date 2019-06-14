3efbd557c46248520837638b26eac117f06a1891
aherve <mail@aurelien-herve.com>
2019-06-14 23:23:50 +0200

    update tracking code

--- frontend/src/app/analytics.service.ts
+++ frontend/src/app/analytics.service.ts
@@ -14,12 +14,16 @@ export class AnalyticsService {
     if (!('analyticsToken' in environment)) { return }
     const script = document.createElement('script')
     script.text = `
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)
-      },i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-      ga('create', '${environment["analyticsToken"]}', 'auto');
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=${environment.analyticsToken}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${environment.analyticsToken}');
+</script>
+
     `
     document.body.appendChild(script)
   }
